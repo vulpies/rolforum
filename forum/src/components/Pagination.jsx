2d943978210fcce56cca7607f028e916bdb4b300
vulpies <anonim616@gmail.com>
2022-08-15 17:35:24 +0300

    pagination

---
+++ forum/src/components/Pagination.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react'
+import ReactPaginate from 'react-paginate';
+import OneEpi from './episodes/oneEpi';
+
+const Pagination = (props) => {
+	const [currentItems, setCurrentItems] = useState([]);
+	const [pageCount, setPageCount] = useState(0);
+	const [itemOffset, setItemOffset] = useState(0);
+	const itemsPerPage = 2
+
+	console.log(props, 'props')
+	console.log(pageCount, 'pageCount')
+	console.log(currentItems, 'currentItems')
+
+	useEffect(() => {
+		const endOffset = itemOffset + itemsPerPage;
+		setCurrentItems(props.data.slice(itemOffset, endOffset));
+		setPageCount(Math.ceil(props.data.length / itemsPerPage));
+	}, [itemOffset, itemsPerPage, props.data]);
+
+	const handlePageClick = (event) => {
+		const newOffset = (event.selected * itemsPerPage) % props.data.length;
+		setItemOffset(newOffset);
+	};
+
+	return (
+		<>
+			{props.children}
+
+			<ReactPaginate
+				breakLabel="..."
+				nextLabel="next >"
+				onPageChange={handlePageClick}
+				marginPagesDisplayed={1}
+				pageRangeDisplayed={2}
+				pageCount={pageCount}
+				previousLabel="< back"
+				renderOnZeroPageCount={null}
+				containerClassName='pagination__container'
+				activeLinkClassName='pagination__active-page'
+				previousLinkClassName="pagination__prev-link"
+				nextLinkClassName="pagination__next-link"
+				previousClassName='pagination__prev'
+				nextClassName='pagination__next'
+				pageClassName='pagination__page'
+				disabledLinkClassName='pagination__disabled'
+			/>
+		</>
+	);
+}
+
+export default Pagination
--- forum/src/components/episodes/oneEpi.jsx
+++ forum/src/components/episodes/oneEpi.jsx
@@ -6,6 +6,7 @@ import ModalEpi from '../forms/modalEpi'
 import { useTranslation } from "react-i18next";
 import { BsSearch } from "react-icons/bs";
 import { CgClose } from "react-icons/cg";
+import Pagination from '../Pagination'
 
 const OneEpi = () => {
 	const { t } = useTranslation();
@@ -22,59 +23,61 @@ const OneEpi = () => {
 
 	return (
 		<>
-			<div className='epi-search'>
-				<form className="epi-search__form">
-					<input type="text"
-						value={value}
-						placeholder="Поиск по названиям..."
-						className="epi-search__input"
-						onChange={(e) => setValue(e.target.value)}
-					/>
-				</form>
-				<p className='epi-search__btn-search'><BsSearch /></p>
-				<button className='epi-search__btn-close' onClick={() => setValue('')}><CgClose /></button>
-			</div>
-
-			{info && filteredEpies?.map((item) => (
-				<div className='epi-wrapper' key={item.id}>
-					<div className='epi-text-info'>
-						<a href={`/episodes/${item.id}`} className='epi-title'>
-							<span className='epi-fandom'>[{item.fandoms.length > 1 ? item.fandoms.join(', ') : item.fandoms[0]}]</span> — <span className='epi-name'>{item.title}</span>
-						</a>
-
-						<ModalEpi
-							show={show}
-							id={item.id}
-							link={`/episodes/${item.id}`}
-							epiName={item.title}
-							fandom={item.fandoms.length > 1 ? item.fandoms.join(', ') : item.fandoms[0]}
-							image={item.image}
-							members={item.characters.map(c => (c.mask ? c.mask : c.name))}
-							text={item.summary}
-							onClose={() => setShow(false)}
+			<Pagination data={filteredEpies}>
+				<div className='epi-search'>
+					<form className="epi-search__form">
+						<input type="text"
+							value={value}
+							placeholder="Поиск по названиям..."
+							className="epi-search__input"
+							onChange={(e) => setValue(e.target.value)}
 						/>
-
-						<button
-							className='btns btns-common'
-							onClick={() => setShow(item.id)}
-						>
-							{t("components.oneEpi.view")}
-						</button>
-
-					</div>
-
-					<div className='epi-user-info'>
-						<div className='epi-user-info__avatar'>
-							<img src={item?.last_author?.avatar} alt='' onClick={() => navigate(`/characters/${item.last_author.id}`)} />
-						</div>
-						{item.last_post_id ?
-							<p><a href={`/episodes/${item.id}#p${item.last_post_id}`}>{item?.last_updated_at}</a></p>
-							:
-							<p>{item?.last_updated_at}</p>
-						}
-					</div>
+					</form>
+					<p className='epi-search__btn-search'><BsSearch /></p>
+					<button className='epi-search__btn-close' onClick={() => setValue('')}><CgClose /></button>
 				</div>
-			))}
+
+				{info && filteredEpies?.map((item) => (
+					<div className='epi-wrapper' key={item.id}>
+						<div className='epi-text-info'>
+							<a href={`/episodes/${item.id}`} className='epi-title'>
+								<span className='epi-fandom'>[{item.fandoms.length > 1 ? item.fandoms.join(', ') : item.fandoms[0]}]</span> — <span className='epi-name'>{item.title}</span>
+							</a>
+
+							<ModalEpi
+								show={show}
+								id={item.id}
+								link={`/episodes/${item.id}`}
+								epiName={item.title}
+								fandom={item.fandoms.length > 1 ? item.fandoms.join(', ') : item.fandoms[0]}
+								image={item.image}
+								members={item.characters.map(c => (c.mask ? c.mask : c.name))}
+								text={item.summary}
+								onClose={() => setShow(false)}
+							/>
+
+							<button
+								className='btns btns-common'
+								onClick={() => setShow(item.id)}
+							>
+								{t("components.oneEpi.view")}
+							</button>
+
+						</div>
+
+						<div className='epi-user-info'>
+							<div className='epi-user-info__avatar'>
+								<img src={item?.last_author?.avatar} alt='' onClick={() => navigate(`/characters/${item.last_author.id}`)} />
+							</div>
+							{item.last_post_id ?
+								<p><a href={`/episodes/${item.id}#p${item.last_post_id}`}>{item?.last_updated_at}</a></p>
+								:
+								<p>{item?.last_updated_at}</p>
+							}
+						</div>
+					</div>
+				))}
+			</Pagination>
 		</>
 	)
 }
